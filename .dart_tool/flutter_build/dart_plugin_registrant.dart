f433e35a4c65f08e1816d640d2cbeb926223eb60
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2024-03-14 10:57:29 -0700

    update check connectivity

--- .dart_tool/flutter_build/dart_plugin_registrant.dart
+++ .dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -8,6 +8,7 @@
 import 'dart:io'; // flutter_ignore: dart_io_import.
 import 'package:local_auth_android/local_auth_android.dart';
 import 'package:local_auth_darwin/local_auth_darwin.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:local_auth_windows/local_auth_windows.dart';
 
 @pragma('vm:entry-point')
@@ -36,6 +37,15 @@ class _PluginRegistrant {
       }
 
     } else if (Platform.isLinux) {
+      try {
+        ConnectivityPlusLinuxPlugin.registerWith();
+      } catch (err) {
+        print(
+          '`connectivity_plus` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isMacOS) {
     } else if (Platform.isWindows) {
       try {
--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,26 @@ import 'package:flutter/material.dart';
 import 'splash_screen.dart'; // Import your splash screen widget
 // import 'hello.dart';
 import 'role.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 
 void main() {
   runApp(MyApp());
+   // Check network connectivity when the app starts
+  checkConnectivity();
 }
+
+
+void checkConnectivity() async {
+  var connectivityResult = await Connectivity().checkConnectivity();
+  if (connectivityResult == ConnectivityResult.mobile) {
+    print('Mobile data connection available');
+  } else if (connectivityResult == ConnectivityResult.wifi) {
+    print('WiFi connection available');
+  } else {
+    print('No active network connection');
+  }
+}
+
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
