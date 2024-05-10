5a3abd8e251c0cf564f6d16cf13d628c9740e796
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2024-05-10 01:30:19 -0700

    change paswword update

--- lib/changePassword.dart
+++ lib/changePassword.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import FirebaseAuth for password change
 
 class ChangePasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController currentPasswordController = TextEditingController();
+    TextEditingController newPasswordController = TextEditingController();
+    TextEditingController confirmPasswordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Change Password',
-          style: TextStyle(color: Colors.black,
-          fontSize: 30),
+          style: TextStyle(color: Colors.black, fontSize: 30),
         ),
         backgroundColor: const Color(0xFF91E1DB), // Set background color of the app bar
       ),
@@ -19,6 +23,7 @@ class ChangePasswordPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TextFormField(
+              controller: currentPasswordController,
               decoration: InputDecoration(
                 labelText: 'Current Password',
                 labelStyle: TextStyle(color: Colors.black),
@@ -30,6 +35,7 @@ class ChangePasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             TextFormField(
+              controller: newPasswordController,
               decoration: InputDecoration(
                 labelText: 'New Password',
                 labelStyle: TextStyle(color: Colors.black),
@@ -41,6 +47,7 @@ class ChangePasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             TextFormField(
+              controller: confirmPasswordController,
               decoration: InputDecoration(
                 labelText: 'Confirm New Password',
                 labelStyle: TextStyle(color: Colors.black),
@@ -52,12 +59,39 @@ class ChangePasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Implement logic to change password
+              onPressed: () async {
+                String currentPassword = currentPasswordController.text;
+                String newPassword = newPasswordController.text;
+                String confirmPassword = confirmPasswordController.text;
+
+                if (newPassword != confirmPassword) {
+                  // Passwords don't match
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Passwords don't match")));
+                  return;
+                }
+
+                try {
+                  User? user = FirebaseAuth.instance.currentUser;
+                  if (user != null) {
+                    await user.reauthenticateWithCredential(EmailAuthProvider.credential(email: user.email!, password: currentPassword));
+                    await user.updatePassword(newPassword);
+
+                    // Password updated successfully
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Password updated successfully")));
+                  }
+                } catch (e) {
+                  // Handle password change errors
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error changing password: $e")));
+                }
               },
-              child: Text('Submit', style: TextStyle(color: Colors.black,
-              fontFamily: 'BalooBhai2',
-              fontSize: 20)),
+              child: Text(
+                'Submit',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontFamily: 'BalooBhai2',
+                  fontSize: 20,
+                ),
+              ),
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFFFFFFFF), // Set button background color to match app bar
               ),
--- lib/vendorChangePassword.dart
+++ lib/vendorChangePassword.dart
@@ -1,15 +1,91 @@
+// import 'package:flutter/material.dart';
+
+// class ChangePasswordPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text(
+//           'Change Password',
+//           style: TextStyle(color: Colors.black,
+//           fontFamily: 'BalooBhai2',
+//           fontSize: 30),
+//         ),
+//         backgroundColor: const Color(0xFFC39ADC), // Set background color of the app bar
+//       ),
+//       backgroundColor: const Color(0xFFC39ADC), // Set background color of the scaffold body
+//       body: Padding(
+//         padding: const EdgeInsets.all(20),
+//         child: Column(
+//           crossAxisAlignment: CrossAxisAlignment.center,
+//           children: [
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'Current Password',
+//                 labelStyle: TextStyle(color: Colors.black),
+//                 enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//               ),
+//               style: TextStyle(color: Colors.black),
+//               obscureText: true,
+//             ),
+//             SizedBox(height: 20),
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'New Password',
+//                 labelStyle: TextStyle(color: Colors.black),
+//                 enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//               ),
+//               style: TextStyle(color: Colors.white),
+//               obscureText: true,
+//             ),
+//             SizedBox(height: 20),
+//             TextFormField(
+//               decoration: InputDecoration(
+//                 labelText: 'Confirm New Password',
+//                 labelStyle: TextStyle(color: Colors.black),
+//                 enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
+//               ),
+//               style: TextStyle(color: Colors.black),
+//               obscureText: true,
+//             ),
+//             SizedBox(height: 20),
+//             ElevatedButton(
+//               onPressed: () {
+//                 // Implement logic to change password
+//               },
+//               child: Text('Submit', style: TextStyle(color: Colors.black,
+//               fontFamily: 'BalooBhai2',
+//               fontSize: 20)),
+//               style: ElevatedButton.styleFrom(
+//                 backgroundColor: const Color(0xFF91E1DB), // Set button background color to match app bar
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart'; // Import FirebaseAuth for password change
 
 class ChangePasswordPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    TextEditingController currentPasswordController = TextEditingController();
+    TextEditingController newPasswordController = TextEditingController();
+    TextEditingController confirmPasswordController = TextEditingController();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(
           'Change Password',
-          style: TextStyle(color: Colors.black,
-          fontFamily: 'BalooBhai2',
-          fontSize: 30),
+          style: TextStyle(color: Colors.black, fontFamily: 'BalooBhai2', fontSize: 30),
         ),
         backgroundColor: const Color(0xFFC39ADC), // Set background color of the app bar
       ),
@@ -20,6 +96,7 @@ class ChangePasswordPage extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             TextFormField(
+              controller: currentPasswordController,
               decoration: InputDecoration(
                 labelText: 'Current Password',
                 labelStyle: TextStyle(color: Colors.black),
@@ -31,17 +108,19 @@ class ChangePasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             TextFormField(
+              controller: newPasswordController,
               decoration: InputDecoration(
                 labelText: 'New Password',
                 labelStyle: TextStyle(color: Colors.black),
                 enabledBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
                 focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.black)),
               ),
-              style: TextStyle(color: Colors.white),
+              style: TextStyle(color: Colors.black),
               obscureText: true,
             ),
             SizedBox(height: 20),
             TextFormField(
+              controller: confirmPasswordController,
               decoration: InputDecoration(
                 labelText: 'Confirm New Password',
                 labelStyle: TextStyle(color: Colors.black),
@@ -53,12 +132,34 @@ class ChangePasswordPage extends StatelessWidget {
             ),
             SizedBox(height: 20),
             ElevatedButton(
-              onPressed: () {
-                // Implement logic to change password
+              onPressed: () async {
+                String currentPassword = currentPasswordController.text;
+                String newPassword = newPasswordController.text;
+                String confirmPassword = confirmPasswordController.text;
+
+                if (newPassword != confirmPassword) {
+                  // Passwords don't match
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Passwords don't match")));
+                  return;
+                }
+
+                try {
+                  User? user = FirebaseAuth.instance.currentUser;
+                  if (user != null) {
+                    await user.updatePassword(newPassword);
+
+                    // Password updated successfully
+                    ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Password updated successfully")));
+                  }
+                } catch (e) {
+                  // Handle password change errors
+                  ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Error changing password: $e")));
+                }
               },
-              child: Text('Submit', style: TextStyle(color: Colors.black,
-              fontFamily: 'BalooBhai2',
-              fontSize: 20)),
+              child: Text(
+                'Submit',
+                style: TextStyle(color: Colors.black, fontFamily: 'BalooBhai2', fontSize: 20),
+              ),
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFF91E1DB), // Set button background color to match app bar
               ),
