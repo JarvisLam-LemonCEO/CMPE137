d75e0f414e552af792b259b2bf37bbeb934eab43
Jarvis Lam <102021121+JarvisLam-LemonCEO@users.noreply.github.com>
2024-05-14 02:43:39 -0700

    map function updates

--- lib/radar.dart
+++ lib/radar.dart
@@ -11,6 +11,7 @@ class Vardar extends StatefulWidget {
 
 class _VardarState extends State<Vardar> {
   late final ListOfVendors allVendors;
+  late GoogleMapController mapController; // Add GoogleMapController instance
 
   @override 
   void initState(){
@@ -25,9 +26,6 @@ class _VardarState extends State<Vardar> {
         preferredSize: Size.fromHeight(0), // Set appBar height to 0
         child: AppBar(
           automaticallyImplyLeading: false, // Set automaticallyImplyLeading to false
-          // backgroundColor: const Color.fromRGBO(222, 173, 0, 100),
-          // centerTitle: true,
-          // title: const Text('Vendor Locator'),
         ),
       ),
       body: GoogleMap(
@@ -36,22 +34,31 @@ class _VardarState extends State<Vardar> {
           target: allVendors.vendors.first.location,
           zoom: 15.0,
         ),
-        markers: allVendors.vendors.map((vendors){
+        markers: Set<Marker>.of(allVendors.vendors.map((vendor) {
           return Marker(
-            markerId: MarkerId(vendors.name),
-            position: vendors.location,
+            markerId: MarkerId(vendor.name),
+            position: vendor.location,
             infoWindow: InfoWindow(
-              title: vendors.name,
-              snippet: vendors.rating,
+              title: vendor.name,
+              snippet: vendor.rating,
             ),
           );
-        }).toSet(),
+        })),
+        zoomControlsEnabled: true, // Enable zoom controls
+        myLocationEnabled: true, // Enable current location button
+        myLocationButtonEnabled: true, // Enable current location button
+        onTap: _onMapTapped, // Handle map tap events
       ),
     );
   }
 
   void _onMapCreated(GoogleMapController controller) {
-    // Use the controller for map functionalities
+    setState(() {
+      mapController = controller;
+    });
+  }
+
+  void _onMapTapped(LatLng latLng) {
+    // Handle tap events on the map
   }
 }
-
